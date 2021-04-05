8ae0ca7cbf124d12648f3876db00e78a80312c83
Kuriya <yunimaru.aaa@mail.com>
2021-04-05 13:09:34 +0900

    【更新】mute状態で切断した場合にmute状態が残る不具合を修正

--- auto_muter/channel_mute_bot.py
+++ auto_muter/channel_mute_bot.py
@@ -10,7 +10,7 @@ from discord.ext import commands
 TOKEN = "BOT_TOKEN"
 
 # MEMO: muter channel id
-muter_channel_ids = [00000000000000000]
+mute_channel_ids = [00000000000000000]
 
 client = commands.Bot(command_prefix='.')
 
@@ -23,12 +23,23 @@ async def on_ready():
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    global muter_channel_ids
-    print('on_voice_state_update')
+    global mute_channel_ids
+
+    # --------------------------------------------------------------------------------
+    # voice state cannot edit after being "disconnect" voice channel.
+    # voice state unmute when "connect".
+    # --------------------------------------------------------------------------------
+
+    # on_disconnect
+    if member.voice == None:
+        return
+
+    # mute:     when connect to mute channels.
+    # unmute:   when "new connect" or connect to other channels.
     if before.channel != after.channel:
-        if after.channel != None and after.channel.id in muter_channel_ids:
+        if after.channel != None and after.channel.id in mute_channel_ids:
             await member.edit(mute=True)
-        elif before.channel != None and before.channel.id in muter_channel_ids:
+        elif before.channel == None or before.channel.id in mute_channel_ids:
             await member.edit(mute=False)
 
 client.run(TOKEN)
